8f4e0dcd4dd31af488922cbf01be1bfb8e57f059
Galen Palmer <palmergs@gmail.com>
2015-05-15 14:03:23 -0400

    use body for document on post

--- searchhandler.go
+++ searchhandler.go
@@ -5,11 +5,19 @@ import (
 	"encoding/json"
 	"strings"
 	"github.com/palmergs/tokensearch"
+	"io/ioutil"
 )
 
 func SearchHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	doc := r.Form.Get("q")
+	if doc == "" {
+		defer r.Body.Close()
+		body, err := ioutil.ReadAll(r.Body)
+		if err == nil {
+			doc = string(body)
+		}
+	}
 
 	pool := tokensearch.NewTokenNodeVisitorPool(root)
 	pool.AdvanceThrough(strings.NewReader(doc))
